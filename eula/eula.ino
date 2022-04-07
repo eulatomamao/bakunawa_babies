396214207a173da1609920bee4e91df0791dcda5
eulatomamao <eula.tomamao@gmail.com>
2022-04-07 14:52:21 -0500

    servo update!
    added servo code

--- eula/eula.ino
+++ eula/eula.ino
@@ -1,6 +1,20 @@
+/* AET BAKUNAWA
+ *  Team Name: Bakunawa Babies
+ *  Team Members: Anna Roberts and Eula Tomamao
+ *  Desc: Servo dragon tries to eat the moon but (hopefully) fails
+ */
+
+//servo library
+#include<Servo.h>;
+
+//variables
+Servo myServo
+const int buttonPin = 7;
+
 void setup() {
   // put your setup code here, to run once:
-
+ myServo.attach(9);
+ pinMode(buttonPin, INPUT);
 }
 
 void loop() {
